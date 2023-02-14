6c715cf990dac23f495664647a21e2e3ab0d2550
Roman Guydmn <romanguydmn@gmail.com>
2023-02-14 15:13:55 +0400

    Задача 43 Семинар 6

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первую координату первой точки k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую координату первой точки b1 : ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первую координату второй точки k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую координату второй точки b2 : ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+int x = 0;
+int y = 0;
+    if ((k1==k2) && (b1==b2))
+    {
+        Console.WriteLine($"Прямые совпадают");
+    }
+    else if (k1==k2)
+    {
+        Console.WriteLine($"Прямые не пересекаются");
+    }
+    else
+    {
+        x = (b2-b1)/(k1-k2);
+        y = k1*x + b1;
+        Console.WriteLine($"Прямые пересекаются в точке ({x},{y})");
+    }
